14229c268ec507212d726b9d134fb57068307b7d
Ben-Boyte <zissette1@gmail.com>
2022-04-28 19:12:31 -0500

    added comment

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,31 @@
-// Assignment code here
 
+//put this inside function
+//var random = function(){}
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
+function generatePassword() {
+   
   var promptLength = window.prompt("Choose length of password (8-128 characters)")
 
       if (promptLength === "" || promptLength === null) {
         window.alert("You need to provide a valid entry! Please try again.");
-        return writePassword();
+        return generatePassword();
       }
       
-      if (promptLength > 8 || promptLength < 128) {
+      if (promptLength > 8 && promptLength < 128) {
+
         window.alert("Success");
       }
 
       if (promptLength < 8 || promptLength > 128) {
         window.alert("Denied");
+        return generatePassword();
       }
 
   var promptCaps = window.prompt("Would you like password to contain uppercase letters?")
 
       if (promptCaps === "" || promptCaps === null ) {
         window.alert("You need to provide a valid entry! Please try again.");
-        return writePassword();
+        return generatePassword();
       }
 
       if (promptCaps === "yes" || promptCaps === "YES") {
@@ -33,23 +33,47 @@ function writePassword() {
       }
       if (promptCaps === "no" || promptCaps === "No") {
       window.alert("Denied");
+      return generatePassword();
      }
 
   var promptLow = window.prompt("Would you like password to contain lowercase letters?")
 
      if (promptLow === "" || promptLow === null ) {
        window.alert("You need to provide a valid entry! Please try again.");
-       return writePassword();
+       return generatePassword();
      }
 
      if (promptLow === "yes" || promptLow === "YES") {
-       window.alert("Success");
+       window.alert("Success"); 
      }
      if (promptLow === "no" || promptLow === "No") {
      window.alert("Denied");
+     return generatePassword();
+
+    }
+
+    var promptSpcl = window.prompt("Would you like password to contain special characters?")
+
+     if (promptSpcl === "" || promptSpcl === null ) {
+       window.alert("You need to provide a valid entry! Please try again.");
+       return generatePassword();
+     }
+
+     if (promptSpcl === "yes" || promptSpcl === "YES") {
+       window.alert("Success"); 
+     }
+     if (promptSpcl === "no" || promptSpcl === "No") {
+     window.alert("Denied");
+     return generatePassword();
+
     }
   
+  }
+    // Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
       
+
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
