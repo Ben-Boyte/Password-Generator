cfb9ed6581d5972f8b19c22fd958faa9fa518fbd
Ben-Boyte <zissette1@gmail.com>
2022-04-30 12:46:18 -0500

    fixed for loop, got the generator working

--- Develop/script.js
+++ Develop/script.js
@@ -1,102 +1,67 @@
 
 
 var generatePassword = function(promptLength, promptCaps, promptLow, promptSpcl, promptNum) {
-  var result = ""
-   var characters = ""; 
+  var result = "";
+   var characters = []; 
       
-    var promptLength = window.prompt("Choose length of password (8-128 characters)")
+    var promptLength = Number(window.prompt("Choose length of password (8-128 characters)"))
 
       if (promptLength === "" || promptLength === null) {
         window.alert("You need to provide a valid entry! Please try again.");
         return generatePassword();
       }
       
-      if (promptLength === "yes" || promptLength === "YES") {
-        characters += promptLength;
-        console.log(characters += promptLength);
-      }
-
       if (promptLength < 8 || promptLength > 128) {
         window.alert("Denied");
         return generatePassword();
       }
 
   var promptCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        window.prompt("Would you like password to contain uppercase letters?")
+        window.confirm("Would you like password to contain uppercase letters?")
 
-      if (promptCaps === "" || promptCaps === null ) {
-        window.alert("You need to provide a valid entry! Please try again.");
-        return generatePassword();
+      if (promptCaps) {
+        characters.push(...promptCaps);
       }
-
-      if (promptCaps === "yes" || promptCaps === "YES") {
-        characters += promptCaps;
-      }
-      if (promptCaps === "no" || promptCaps === "No") {
-      window.alert("Denied");
-     }
+      console.log(characters);
+      
 
      var promptLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
-      window.prompt("Would you like password to contain lowercase letters?")
-
-     if (promptLow === "" || promptLow === null ) {
-       window.alert("You need to provide a valid entry! Please try again.");
-       return generatePassword();
-     }
-
-     if (promptLow === "yes" || promptLow === "YES") {
-       characters += promptLow; 
-     }
-     if (promptLow === "no" || promptLow === "No") {
-     window.alert("Denied");
-    }
+      window.confirm("Would you like password to contain lowercase letters?")
+      if (promptLow) {
+        characters.push(...promptLow);
+      }
+      console.log(promptLow);
 
     var promptSpcl = ["!", "@", "#", "$", "%", "&", "*"];
 
-    window.prompt("Would you like password to contain special characters?")
+    window.confirm("Would you like password to contain special characters?")
 
-     if (promptSpcl === "" || promptSpcl === null ) {
-       window.alert("You need to provide a valid entry! Please try again.");
-       return generatePassword();
-     }
-
-     if (promptSpcl === "yes" || promptSpcl === "YES") {
-       characters += promptSpcl;
-     }
-     if (promptSpcl === "no" || promptSpcl === "No") {
-     window.alert("Denied");
+    if (promptSpcl) {
+      characters.push(...promptSpcl);
     }
+    console.log(promptSpcl);
 
     var promptNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-    window.prompt("Would you like password to contain numbers?")
+    window.confirm("Would you like password to contain numbers?")
 
-     if (promptNum === "" || promptNum === null ) {
-       window.alert("You need to provide a valid entry! Please try again.");
-       return generatePassword();
-     }
-
-     if (promptNum === "yes" || promptNum === "YES") {
-       characters += promptNum;
-     }
-     if (promptNum === "no" || promptNum === "No") {
-     window.alert("Denied");
-  
-     for (var i = 0; i < promptLength; i++) {
-       result += characters;
+    if (promptNum) {
+      characters.push(...promptNum);
     }
+    console.log(promptNum);
   
-  
+  console.log(characters);
+
+  for (let i = 0; i < promptLength; i++) {
+    const random = Math.floor(Math.random() * characters.length);
+    result += characters[random]
+  }
   return result;
 }
-return password;
-}
 
-    // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
       
-
   function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -105,5 +70,4 @@ var generateBtn = document.querySelector("#generate");
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
